41b3a0ff20a5210c8020d328c9bdf1371e9aa581
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-09-25 22:37:58 +0300

    Controllerlar yazıldı.

--- HotelProject.WebApi/Program.cs
+++ HotelProject.WebApi/Program.cs
@@ -1,3 +1,10 @@
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
+using HotelProject.EntityLayer.Concrete;
+
 namespace HotelProject.WebApi
 {
     public class Program
@@ -7,6 +14,29 @@ namespace HotelProject.WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<Context>();
+            builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+            builder.Services.AddScoped<IStaffService, StaffManager>();
+
+            builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+            builder.Services.AddScoped<IRoomService, RoomManager>();
+
+            builder.Services.AddScoped<IServiceDal, EfServiceDal>();
+            builder.Services.AddScoped<IServiceService, ServiceManager>();
+
+            builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+            builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
+            builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+            builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+
+            builder.Services.AddCors(opt =>
+            {
+                opt.AddPolicy("OtelApiCors", opts =>
+                {
+                    opts.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+                });
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,6 +52,7 @@ namespace HotelProject.WebApi
                 app.UseSwaggerUI();
             }
 
+            app.UseCors("OtelApiCors");
             app.UseAuthorization();
 
 
