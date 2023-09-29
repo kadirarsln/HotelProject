698f4c002c8903a8cd8481b4b974636ff4d6a5b7
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-09-29 13:28:12 +0300

    Personelin eklenmesi işlemi.
    Staff controller post method yazıldı.
    AddStaff model eklendi.

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -27,5 +28,26 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStaff(AddStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5240/api/Staff" , stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("StaffIndex");
+            }
+            return View();
+        }
+
     }
 }
