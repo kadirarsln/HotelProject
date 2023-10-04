01f1e60de9071f3cd27bfb3dc08be37f9146abd9
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-04 16:40:10 +0300

    RegisterController added.
    CreateNewUserDto and Register Controller addded.

---
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.RegisterDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult RegisterIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterIndex(CreateNewUserDto createNewUserDto)
+        {
+            return View();
+        }
+    }
+}
