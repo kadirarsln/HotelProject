3e28043e5b656bd717bab39e97aafa3cfe91676c
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-06 13:33:28 +0300

    Login işlemi
    LoginController added
    LoginIndex added.
    Gerekli düzenlemeler yapıldı.

---
+++ Frontend/HotelProject.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.LoginDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult LOginIndex()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> LoginIndex(LoginUserDto loginUserDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginUserDto.Username, loginUserDto.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("StaffIndex", "Staff");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
