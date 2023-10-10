5f4e99286970357a3dbbc2ec8bd743fd4514cbea
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-10 14:36:04 +0300

    Staff added & Subscribe added.
    Takım arkadaşlarımız kısmı eklendi.
    Mail Bültenine abone olma.

--- Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
+++ Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult _SubscribePartial()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> _SubscribePartial(CreateSubscribeDto createSubscribeDto)
+        {
+            var client = _httpClientFactory.CreateClient();                                          // Consume için istemci oluşturuldu.
+            var responseMessage = await client.GetAsync("http://localhost:5240/api/Subscribe");          // Belirtilen adrese istekte bulunuldu.
+            if (responseMessage.IsSuccessStatusCode)                                                 // Adresten başarılı durum kodu dönerse ,
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();                    // Gelen veriyi değişkene atadık. (JsonData)
+                var values = JsonConvert.DeserializeObject<List<CreateSubscribeDto>>(jsonData);          // Deserialize ile tabloda görülecek formata getirdik.
+                return View(values);
+            }
             return View();
         }
     }
--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.StaffDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TeamPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TeamPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();                                          // Consume için istemci oluşturuldu.
+            var responseMessage = await client.GetAsync("http://localhost:5240/api/Staff");          // Belirtilen adrese istekte bulunuldu.
+            if (responseMessage.IsSuccessStatusCode)                                                 // Adresten başarılı durum kodu dönerse ,
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();                    // Gelen veriyi değişkene atadık. (JsonData)
+                var values = JsonConvert.DeserializeObject<List<ResultStaffDto>>(jsonData);          // Deserialize ile tabloda görülecek formata getirdik.
+                return View(values);
+            }
             return View();
         }
     }
