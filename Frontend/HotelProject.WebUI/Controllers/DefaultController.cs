52c00b021dd6f5dd0130f988a7223af208033fe0
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-10 14:44:10 +0300

    Subscribe partial update.
    Mail sorunu giderildi.

--- Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
+++ Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
@@ -2,6 +2,7 @@
 using HotelProject.WebUI.Dtos.SubscribeDto;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -25,13 +26,13 @@ namespace HotelProject.WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> _SubscribePartial(CreateSubscribeDto createSubscribeDto)
         {
-            var client = _httpClientFactory.CreateClient();                                          // Consume için istemci oluşturuldu.
-            var responseMessage = await client.GetAsync("http://localhost:5240/api/Subscribe");          // Belirtilen adrese istekte bulunuldu.
-            if (responseMessage.IsSuccessStatusCode)                                                 // Adresten başarılı durum kodu dönerse ,
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSubscribeDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5240/api/Subscribe", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
             {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();                    // Gelen veriyi değişkene atadık. (JsonData)
-                var values = JsonConvert.DeserializeObject<List<CreateSubscribeDto>>(jsonData);          // Deserialize ile tabloda görülecek formata getirdik.
-                return View(values);
+                return RedirectToAction("Index","Default");
             }
             return View();
         }
