1a7e66ec5f206c367eccf13c448576e4a5190846
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-11 23:09:46 +0300

    AdminToken oluşturuldu. Yetkili giriş.
    Role bağlı giriş için AdminTokenOluşturma eklendi.
    Controller kısmında admin için controller eklendi.

--- JwtProject/WebApiJwt/Controllers/DefaultController.cs
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -9,16 +9,31 @@ namespace WebApiJwt.Controllers
     [ApiController]
     public class DefaultController : ControllerBase
     {
+        //403 hatası dönüyor yetkiden dolayı.
         [HttpGet("[action]")]
-        public IActionResult Get()
+        public IActionResult TokenOluştur()
         {
             return Ok(new CreateToken().Create());
         }
+        //Admin Token Oluşturuldu.
+        [HttpGet("[action]")]
+        public IActionResult AdminTokenOluştur()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+
         [Authorize]
         [HttpGet("[action]")]
         public IActionResult Get2()
         {
             return Ok("Hoşgeldiniz");
+        }
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Get3()
+        {
+            return Ok("Token Başarılı Bir Şekilde Giriş Yaptı");
         }
     }
 }
--- JwtProject/WebApiJwt/Models/CreateToken.cs
+++ JwtProject/WebApiJwt/Models/CreateToken.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -20,6 +21,24 @@ namespace WebApiJwt.Models
 
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
             return handler.WriteToken(token);
+        }
+        public string TokenCreateAdmin()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+            SymmetricSecurityKey key1 = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key1, SecurityAlgorithms.HmacSha256);
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role,"Admin"),
+                new Claim(ClaimTypes.Role,"Visitor")
+            };
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost",
+                notBefore: DateTime.Now, expires: DateTime.Now.AddSeconds(30), signingCredentials: credentials, claims: claims);
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            return jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
         }
     }
 }
