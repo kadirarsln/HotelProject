3527cf955090f4e78a1e83e04464f19cb70566a2
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-19 19:00:56 +0300

    AdminContact and GetList Message added.
    Admin paneli mesaj sayfası ve gelen mesajları listeleme işlemi yapıldı.
    Controllerlar düzenlendi.

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Reflection.Metadata.Ecma335;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class AdminContactController : Controller
     {
-        public IActionResult InboxIndex()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> InboxIndex()
+        {
+            var client = _httpClientFactory.CreateClient();                                          // Consume için istemci oluşturuldu.
+            var responseMessage = await client.GetAsync("http://localhost:5240/api/Contact");          // Belirtilen adrese istekte bulunuldu.
+            if (responseMessage.IsSuccessStatusCode)                                                 // Adresten başarılı durum kodu dönerse ,
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();                    // Gelen veriyi değişkene atadık. (JsonData)
+                var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);          // Deserialize ile tabloda görülecek formata getirdik.
+                return View(values);
+            }
             return View();
+        }
+
+        public PartialViewResult SidebarAdminContactPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SidebarAdminContactCategoryPartial()
+        {
+            return PartialView();
         }
     }
 }
