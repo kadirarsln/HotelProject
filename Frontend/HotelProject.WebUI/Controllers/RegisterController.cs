c438f23442b4c03d72b8d49f2a488ddc5995dd30
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-26 00:15:52 +0300

    WorkLocation Table added.
    Paketlerde güncelleme ve work location

--- Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -35,7 +35,8 @@ namespace HotelProject.WebUI.Controllers
                 Surname = createNewUserDto.Surname,
                 Email = createNewUserDto.Mail,
                 UserName = createNewUserDto.Username,
-                City = createNewUserDto.City
+                City = createNewUserDto.City,
+                WorkLocationID = 1
             };
             var result = await _userManager.CreateAsync(appUser, createNewUserDto.Password);
             if (result.Succeeded)
